747bf4f7cccc33b4bd82ceda40ecb74be329e154
Hatice Tasci <thtcee366@gmail.com>
2023-04-06 10:48:00 +0200

    2

---
+++ src/main/java/Arrayss/Q02.java
@@ -0,0 +1,20 @@
+package Arrayss;
+
+public class Q02 {
+    public static void main(String[] args) {
+
+
+
+        int arr[] ={3,2,3,4,4,5};
+        int cont =0;
+        for (int i =0;i<arr.length;i++){
+            if (arr[i]==3){
+                cont++;
+
+            }
+
+        }
+        System.out.println("cont = " + cont);
+
+    }
+}
