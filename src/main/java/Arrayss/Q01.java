ef838f1818c8a6526addb398b1e2e86043797d98
Hatice Tasci <thtcee366@gmail.com>
2023-04-06 10:14:50 +0200

    1

---
+++ src/main/java/Arrayss/Q01.java
@@ -0,0 +1,24 @@
+package Arrayss;
+
+import java.util.Arrays;
+
+public class Q01 {
+    public static void main(String[] args) {
+
+        int arr []={2,11,0,23,7};
+
+        Arrays.sort(arr);
+
+        for (int i=4;i>=0;i--){
+
+            System.out.println(arr[i]  + " ");
+
+        }
+
+
+
+
+
+
+    }
+}
